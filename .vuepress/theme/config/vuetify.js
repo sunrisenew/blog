577ebc7a801d19ad4af8dceb3d931ebc307c4b0f
sunrisenew <sunrisenew@foxmail.com>
2020-05-11 19:50:37 +0800

    fix(blog): upgrade dependencies and change the accent color

--- .vuepress/theme/config/vuetify.js
+++ .vuepress/theme/config/vuetify.js
@@ -1,17 +1,12 @@
 import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify'
-import colors from 'vuetify/lib/util/colors'
 
 function install(Vue, options) {
   Vue.use(Vuetify)
   options && (Object.assign(options, {
     vuetify: new Vuetify({
       theme: {
-        themes: {
-          light: {
-            primary: colors.orange.base
-          }
-        }
+        disable: true
       }
     })
   }))
