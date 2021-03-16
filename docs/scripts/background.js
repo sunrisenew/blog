12c8887f3ffee40f7a3f93879e8e74daed5ac0b4
sunrisenew <sunrisenew@foxmail.com>
2021-03-16 22:41:15 +0800

    docs(blog): new build

--- docs/scripts/background.js
+++ docs/scripts/background.js
@@ -959,7 +959,7 @@ SOFTWARE.
   let sunrays
   let sunraysTemp
 
-  let ditheringTexture = createTextureAsync('LDR_LLL1_0.png')
+  let ditheringTexture = createTextureAsync(`${window.BASE_CDN_URL}/images/LDR_LLL1_0.png`)
 
   const blurProgram = new Program(blurVertexShader, blurShader)
   const copyProgram = new Program(baseVertexShader, copyShader)
